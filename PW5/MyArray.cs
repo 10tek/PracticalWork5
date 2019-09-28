e969a409c910ab7f709fa85fe58be1d5d85c96c7
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-28 16:25:24 +0600

    Добавлены классы User с перегрузкой == и !=, MyArray с перегрузкой < и >.

---
+++ PW5/MyArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW5
+{
+    class MyArray
+    {
+        private int[] numberArray;
+
+        public MyArray()
+        {
+            numberArray = new int[5];
+        }
+
+        public int this[int index]
+        {
+
+            get
+            {
+                if (index >= numberArray.Length)
+                    throw new IndexOutOfRangeException("Wrong index!");
+                else
+                    return numberArray[index];
+            }
+            set
+            {
+                if (index >= numberArray.Length)
+                    throw new IndexOutOfRangeException("Wrong index!");
+                else
+                    numberArray[index] = value;
+            }
+        }
+
+        public int GetSum()
+        {
+            return numberArray.Sum();
+        }
+
+        public static bool operator <(MyArray myFirstArray, MyArray mySecondArray)
+        {
+            return myFirstArray.GetSum() < mySecondArray.GetSum();
+        }
+
+        public static bool operator >(MyArray myFirstArray, MyArray mySecondArray)
+        {
+            return myFirstArray.GetSum() > mySecondArray.GetSum();
+        }
+    }
+
+
+}
---
+++ PW5/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW5
+{
+    public class User
+    {
+        public string FIO { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static bool operator ==(User firstUser, User secondUser)
+        {
+            return firstUser.FIO == secondUser.FIO;
+        }
+
+        public static bool operator !=(User firstUser, User secondUser)
+        {
+            return !(firstUser == secondUser);
+        }
+    }
+}
